f4f4f7928fa71d2a2ac19dd8e80c17be630adb5e
jenamndz <jenamndz18@gmail.com>
2026-06-21 01:09:30 +0800

    Added getter methods for name and age.

--- school_management_system.py
+++ school_management_system.py
@@ -4,3 +4,9 @@ class Person(ABC):
     def __init__(self, name, age):
         self.__name = name
         self.__age = age
+        
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
