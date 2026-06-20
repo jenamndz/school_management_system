aa617fd8c16893708fa9cb29eb7c81aaa4e38dca
jenamndz <jenamndz18@gmail.com>
2026-06-21 01:10:18 +0800

    Added setter methods for name and age.

--- school_management_system.py
+++ school_management_system.py
@@ -4,9 +4,15 @@ class Person(ABC):
     def __init__(self, name, age):
         self.__name = name
         self.__age = age
-        
+
     def get_name(self):
         return self.__name
 
     def get_age(self):
         return self.__age
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_age(self, age):
+        self.__age = age
